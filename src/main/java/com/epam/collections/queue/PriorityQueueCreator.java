8acbaaf5174e1c45eedf33eed5fe5007dcbce202
emmanuel hernandez <emmanuelhern00@gmail.com>
2023-01-10 01:02:06 -0400

    refactoring the Comparator constructor for an equivalent lambda expression in the PriorityQueueCreator class

--- src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
+++ src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
@@ -6,12 +6,7 @@ import java.util.PriorityQueue;
 
 public class PriorityQueueCreator {
     public PriorityQueue<String> createPriorityQueue(List<String> firstList, List<String> secondList) {
-        PriorityQueue<String> createdQueue = new PriorityQueue<>(new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                return o2.compareTo(o1);
-            }
-        });
+        PriorityQueue<String> createdQueue = new PriorityQueue<>( (o1,o2) -> o2.compareTo(o1));
         createdQueue.addAll(firstList);
         createdQueue.addAll(secondList);
         return createdQueue;
